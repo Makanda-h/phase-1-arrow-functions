5f11da5db6d4f0d611451807005d8aa5a1518083
Hope <hopemwaura39@gmail.com>
2024-06-28 11:52:29 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function (parameter1, parameter2) {
+     return parameter1 / parameter2;
+}
+divide (2000,100);
+
+const square = x => x * x;
+
+const add = (parameter3, parameter4) => parameter3 + parameter4;
